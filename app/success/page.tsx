abf2783f4ecc69d6c31bbf7c006ed75171f5b8eb
Davide Tondelli <davide@MacBook-Pro-di-Davide.local>
2026-03-20 18:08:42 +0100

    fix success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,26 +1,14 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+export const dynamic = "force-dynamic";
+
+import { Suspense } from "react";
+import SuccessContent from "./SuccessContent";
 
 export default function SuccessPage() {
-  const params = useSearchParams();
-  const image = params.get("image");
-
   return (
-    <div style={{ padding: 40, textAlign: "center" }}>
-      <h1>Pagamento completato</h1>
-
-      {image && (
-        <>
-          <img src={image} style={{ width: 300 }} />
-
-          <br /><br />
-
-          <a href={image} download>
-            <button>Scarica immagine</button>
-          </a>
-        </>
-      )}
-    </div>
+    <Suspense fallback={<div style={{ padding: 40 }}>Loading...</div>}>
+      <SuccessContent />
+    </Suspense>
   );
 }
